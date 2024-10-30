f8c149e758ce05fec02aa576e64b668e643cb42c
Colton Bassett <coltonbassett@gmail.com>
2024-10-30 14:05:03 -0700

    Commented fetch request for pokemon id, name, types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
 import { Flex, Text, Button } from "@radix-ui/themes";
 
-export default function Home() {
+export default async function Home() {
+	// https://pokeapi.co/api/v2/pokemon/${id}
+	// "https://pokeapi.co/api/v2/pokemon?limit=2&offset=0"
+
+	// let response = await fetch("https://pokeapi.co/api/v2/pokemon/1/");
+	// let data = await response.json();
+	// console.log(data.id);
+	// console.log(data.name);
+	// console.log(data.types[0].type.name);
+	// console.log(data.types[1].type.name);
+
+	// try {
+	// 	for (let id = 1; id <= 3; id++) {
+	// 		let response = await fetch(
+	// 			`https://pokeapi.co/api/v2/pokemon/${id}/`
+	// 		);
+	// 		let data = await response.json();
+	// 		console.log(data.id);
+	// 		console.log(data.name);
+	// 		console.log(data.types[0].type.name);
+	// 		console.log(data.types[1].type.name);
+	// 	}
+	// } catch (error) {
+	// 	console.error("Error fetching Pokémon names:", error);
+	// }
+
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
