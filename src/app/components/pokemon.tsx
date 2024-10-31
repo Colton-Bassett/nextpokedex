5043a0bd9386b32341684013ef83e03925543353
Colton Bassett <coltonbassett@gmail.com>
2024-10-31 16:44:19 -0700

    Moved Pokemon and PokemonList into their own component. Fixed layout on smaller screens

---
+++ src/app/components/pokemon.tsx
@@ -0,0 +1,46 @@
+import { Card, Flex, Text, Grid } from "@radix-ui/themes";
+import Image from "next/image";
+
+export default function Pokemon({
+	id,
+	name,
+	type,
+}: {
+	id: number;
+	name: string;
+	type: string;
+}) {
+	return (
+		<Card key={id}>
+			<Flex direction="column" align="center">
+				<Image
+					className="dark:invert"
+					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+					alt={`${name} sprite`}
+					width={90}
+					height={90}
+					priority
+				/>
+				<Text>{name}</Text>
+				<Text>{type}</Text>
+			</Flex>
+		</Card>
+	);
+}
+
+export function PokemonList({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<Grid
+			columns={{ initial: "1", sm: "2", md: "3" }}
+			gap="3"
+			rows="4"
+			width="auto"
+		>
+			{children}
+		</Grid>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import { Card, Flex, Grid, Text } from "@radix-ui/themes";
+import { Flex } from "@radix-ui/themes";
 import GetPokemonButton from "./components/getPokemonButton";
-import { mockSlowRequest } from "./actions/actions";
-import { sql } from "@vercel/postgres";
-import Image from "next/image";
+import Pokemon, { PokemonList } from "./components/pokemon";
 
 export default async function Home() {
 	// const { rows } =
 	// 	await sql`SELECT * FROM pokedex_pokemon ORDER BY RANDOM() LIMIT 12`;
 	// console.log(rows);
+
+	// const fetchPokemon = async () => {
+	// 	const data = await getAllPokemonFromDB();
+	// 	if (data) {
+	// 		console.log(data);
+	// 	} else {
+	// 		console.error("Failed to fetch Pokémon:");
+	// 	}
+	// };
 
 	const rows = [
 		{ id: 55, name: "Golduck", type: "Water" },
@@ -24,37 +31,20 @@ export default async function Home() {
 		{ id: 45, name: "Vileplume", type: "GrassPoison" },
 	];
 
-	// const fetchPokemon = async () => {
-	// 	const data = await getAllPokemonFromDB();
-	// 	if (data) {
-	// 		console.log(data);
-	// 	} else {
-	// 		console.error("Failed to fetch Pokémon:");
-	// 	}
-	// };
-
 	return (
 		<div className="flex items-center justify-items-center min-h-screen p-8 pb-20 min-w-full">
 			<main className="flex flex-col items-center max-w-5xl w-full m-auto px-7">
 				<Flex direction="column" gap="2">
-					<Grid columns="3" gap="3" rows="4" width="auto">
+					<PokemonList>
 						{rows.map((p) => (
-							<Card key={p.id}>
-								<Flex direction="column" align="center">
-									<Image
-										className="dark:invert"
-										src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
-										alt={`${p.name} sprite`}
-										width={90}
-										height={19}
-										priority
-									/>
-									<Text>{p.name}</Text>
-									<Text>{p.type}</Text>
-								</Flex>
-							</Card>
+							<Pokemon
+								key={p.id}
+								id={p.id}
+								name={p.name}
+								type={p.type}
+							/>
 						))}
-					</Grid>
+					</PokemonList>
 					<GetPokemonButton />
 				</Flex>
 			</main>
