ec6beb732e0d0604b65571a9f3bc19e856664b5e
Colton Bassett <coltonbassett@gmail.com>
2024-11-04 16:54:42 -0800

    Yassified (blurr'd) the nav

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -3,21 +3,27 @@ import Image from "next/image";
 import { Flex } from "@radix-ui/themes";
 
 export default function Nav() {
-	return (
-		<nav>
-			<Flex gap="3" className="h-16 items-center px-7 ">
-				<Link href="/" className="flex  text-2xl gap-3">
-					<Image
-						className="dark:invert"
-						src="/next.svg"
-						alt="Next.js logo"
-						width={90}
-						height={19}
-						priority
-					/>
-					<span className="">Pokedex</span>
-				</Link>
-			</Flex>
-		</nav>
-	);
+  return (
+    <nav
+      className="sticky top-0 z-50 bg-white bg-opacity-80"
+      style={{ backdropFilter: "saturate(180%) blur(5px)" }}
+    >
+      <Flex
+        gap="3"
+        className="h-16 items-center border-b border-neutral-200 px-7"
+      >
+        <Link href="/" className="flex gap-3 text-2xl">
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={90}
+            height={19}
+            priority
+          />
+          <span className="">Pokedex</span>
+        </Link>
+      </Flex>
+    </nav>
+  );
 }
