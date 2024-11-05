073531b31abca891136de8501f360f705c466b7d
Colton Bassett <coltonbassett@gmail.com>
2024-11-05 14:06:35 -0800

    Updated footer mobile styling

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,31 +4,33 @@ import { Flex } from "@radix-ui/themes";
 
 export default function Footer() {
   return (
-    <footer className="border-t">
-      <Flex gap="7" className="m-auto h-32 max-w-4xl items-center px-7">
-        <Link href="/" className="flex gap-3 text-2xl">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={90}
-            height={19}
-            priority
-          />
-          <span className="">Pokedex</span>
-        </Link>
-        <Link
-          href="/"
-          className="text-stone-500 transition-colors duration-200 hover:text-stone-900"
-        >
-          <span className="text-lg">Help</span>
-        </Link>
-        <Link
-          href="/"
-          className="text-stone-500 transition-colors duration-200 hover:text-stone-900"
-        >
-          <span className="text-lg">Disclaimer</span>
-        </Link>
+    <footer className="flex overflow-auto whitespace-nowrap border-t">
+      <Flex className="m-auto h-32 w-full max-w-4xl px-7" align="center">
+        <Flex gap="7" className="min-w-[470px] items-center">
+          <Link href="/" className="flex gap-3 text-2xl">
+            <Image
+              className="dark:invert"
+              src="/next.svg"
+              alt="Next.js logo"
+              width={90}
+              height={19}
+              priority
+            />
+            <span className="">Pokedex</span>
+          </Link>
+          <Link
+            href="/"
+            className="text-stone-500 transition-colors duration-200 hover:text-stone-900"
+          >
+            <span className="text-lg">Help</span>
+          </Link>
+          <Link
+            href="/"
+            className="text-stone-500 transition-colors duration-200 hover:text-stone-900"
+          >
+            <span className="text-lg">Disclaimer</span>
+          </Link>
+        </Flex>
       </Flex>
     </footer>
   );
