9765e27cd19035833e8ad1f71031dd65d0853961
Colton Bassett <coltonbassett@gmail.com>
2024-11-26 10:51:05 -0800

    Added Admin and Github links

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Flex, Text } from "@radix-ui/themes";
+import { Button, Flex, Text } from "@radix-ui/themes";
 
 export default function Nav() {
   return (
@@ -8,7 +8,7 @@ export default function Nav() {
       className="sticky top-0 z-50 bg-white bg-opacity-80"
       style={{ backdropFilter: "saturate(180%) blur(5px)" }}
     >
-      <Flex gap="3" className="h-16 items-center border-b px-7">
+      <Flex gap="3" className="h-16 items-center justify-between border-b px-7">
         <Link href="/" className="flex gap-3 text-2xl">
           <Image
             className="dark:invert"
@@ -20,6 +20,49 @@ export default function Nav() {
           />
           <Text className="">Pokedex</Text>
         </Link>
+        {/* <Link href="/" className="flex gap-3 text-2xl">
+          <Image
+            width={20}
+            height={20}
+            src="/github.svg"
+            alt="Github logo"
+            priority
+          />
+        </Link> */}
+        <Flex gap="4">
+          <Link href="/admin">
+            <Button
+              color="gray"
+              variant="outline"
+              highContrast
+              className="[box-shadow:0_0_0_1px_rgba(0,0,0,0.08)]"
+            >
+              <span>
+                <svg
+                  aria-label="Vercel logomark"
+                  height="13"
+                  role="img"
+                  viewBox="0 0 74 64"
+                >
+                  <path
+                    d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z"
+                    fill="171717"
+                  ></path>
+                </svg>
+              </span>
+              Admin
+            </Button>
+          </Link>
+          <Link
+            href="https://github.com/Colton-Bassett"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button color="gray" variant="solid" highContrast className="">
+              Github
+            </Button>
+          </Link>
+        </Flex>
       </Flex>
     </nav>
   );
